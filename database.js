26cd3e0b3798f55380668c7682a955199758e29f
Meibelle Medina <113251217+meibelle-medina13@users.noreply.github.com>
2024-11-15 19:27:04 +0800

    updated database file

--- database.js
+++ database.js
@@ -1,15 +1,17 @@
 import mysql from 'mysql2'
 
-const DATABASE_HOST = 'localhost'
-const DATABASE_USERNAME = 'root'
-const DATABASE_PASSWORD = ''
+const DATABASE_HOST = 'mysql-17d6ea4e-tinythinker.f.aivencloud.com'
+const DATABASE_USERNAME = 'avnadmin'
+const DATABASE_PASSWORD = 'AVNS_2c14i9EDNnLxLr2d2A3'
 const DATABASE_NAME = 'tinythinker'
+const PORT = 17267
 
 const databaseInstance = mysql.createPool({
   host: DATABASE_HOST,
   user: DATABASE_USERNAME,
   password: DATABASE_PASSWORD,
-  database: DATABASE_NAME
+  database: DATABASE_NAME,
+  port: PORT
 })
 
 export default databaseInstance
