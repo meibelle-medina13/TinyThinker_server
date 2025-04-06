bfe592f929d140b64a3b9b34c01890c84dab3f14
Meibelle Medina <113251217+meibelle-medina13@users.noreply.github.com>
2025-04-06 17:15:27 +0800

    Statistics Endpoints Updates

--- models/statistic.model.js
+++ models/statistic.model.js
@@ -1,28 +1,22 @@
 import databaseInstance from '../database.js'
 
-function _sanitize(text) {
-    if (typeof text === "number") {
-        return text
-    }
-    return text.replace(/([^a-z-A-Z-0-9 .@_'])+/g, '')
-}
-
 function get(user_ID, theme_number, offset = 0, limit = 50) {
     return new Promise((resolve, reject) => {
-        
         if (user_ID && theme_number) {
             if (/\D+/g.test(theme_number)) {
-            console.log('[STATISTICS] Invalid Query', theme_number)
-            resolve([])
+                console.log('[GET STATISTICS] Invalid Query', theme_number)
+                resolve({
+                    "message": '[GET STATISTICS] Invalid Query'
+                })
             }
     
             const theme = parseInt(theme_number, 10)
             const userID = parseInt(user_ID, 10)
-            databaseInstance.query(`SELECT level_num, scores FROM scores WHERE theme_num = ? AND user_ID = ?`, [theme, userID], (err, results, fields) => {
-            if (err) reject(err)
-    
-            console.log(results.length)
-            resolve(results)
+            databaseInstance.query(`SELECT level_num, scores FROM scores WHERE theme_num = ? AND user_ID = ?`, [theme, userID], 
+            (err, results, fields) => {
+                if (err) reject(err)
+                console.log(results.length)
+                resolve(results)
             })
         }
     })
